2485148edc08e65c19fa90ed618c89f9394731ab
EngieDev <admin@engiedev.net>
2020-05-20 22:59:41 +0000

    Added systemd service

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,3 +2,6 @@
 adduser --shell=/bin/false --no-create-home --gecos "" --system --disabled-password
 mkdir /var/lib/failstats/
 chown failstats /var/lib/failstats/
+
+systemctl daemon-reload
+systemctl enable --now failstats.service
